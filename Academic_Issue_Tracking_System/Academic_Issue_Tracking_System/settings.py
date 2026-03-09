34484e7673699caae3d3f30e940df4fbe2a62463
Emarc480 <marcusemma480@gmail.com>
2026-03-09 16:18:54 +0300

    made the first migration

--- Academic_Issue_Tracking_System/Academic_Issue_Tracking_System/settings.py
+++ Academic_Issue_Tracking_System/Academic_Issue_Tracking_System/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AITS.apps.AitsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
