ae9d5bc2a5da7b3f4d33e3257b922cfebdf65fb7
Emarc480 <marcusemma480@gmail.com>
2026-03-09 07:53:24 +0300

    Changed the database to postgress

--- Academic_Issue_Tracking_System/Academic_Issue_Tracking_System/settings.py
+++ Academic_Issue_Tracking_System/Academic_Issue_Tracking_System/settings.py
@@ -74,8 +74,11 @@ WSGI_APPLICATION = 'Academic_Issue_Tracking_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AITS_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Emma1234',
+        'HOST': 'localhost',
     }
 }
 
