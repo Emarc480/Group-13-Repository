d6459d9de6f9cb3eb1434c656f6574454c65b7a8
Emarc480 <marcusemma480@gmail.com>
2026-03-12 22:11:09 +0300

    DONE making react work

--- Academic_Issue_Tracking_System/frontend/src/index.js
+++ Academic_Issue_Tracking_System/frontend/src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />)
-
-console.log("React started")
