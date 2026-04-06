cc509267f8be28bd6702dd946a9f5a2bc5fa6109
aojangole <arikolojangole@gmail.com>
2026-04-06 06:57:07 +0300

    made class User to inherit from AbstractUser

--- Academic_Issue_Tracking_System/AITS/models.py
+++ Academic_Issue_Tracking_System/AITS/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
+class AbstractUser:
     ROLE_CHOICES = (
         ('student', 'Student'),
         ('registrar', 'Academic Registrar'),
