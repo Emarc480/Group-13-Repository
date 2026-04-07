1c9b82e46011eeaedccaa880783fd19e6ddcbfe3
Emarc480 <marcusemma480@gmail.com>
2026-04-07 13:37:38 +0300

    Working on the dashboard started

---
+++ Academic_Issue_Tracking_System/frontend/src/components/Views/StudentView.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function StudentView() {
+    return (
+        <div>
+            <h2>Student View</h2>
+            <p>This is the student dashboard where students can view their issues, submit new issues, and track the status of their existing issues.</p>
+        </div>
+    );
+}
--- Academic_Issue_Tracking_System/frontend/src/pages/Dashboard.js
+++ Academic_Issue_Tracking_System/frontend/src/pages/Dashboard.js
@@ -1,5 +1,22 @@
 import React from "react";
 
+import StudentView from "../components/Views/StudentView";
+import LecturerView from "../components/Views/LecturerView";
+import HodView from "../components/Views/HodView";
+import RegistrarView from "../components/Views/RegistrarView";
+
 export default function Dashboard() {
-    return <p>This is your dashboard</p>;
-}
+    const role = localStorage.getItem('role');
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+
+            {role === 'student' && (<StudentView />)}
+            {role === 'lecturer' && (<LecturerView />)}
+            {role === 'hod' && (<HodView />)}
+            {role === 'registrar' && (<RegistrarView />)}
+
+        </div>
+    );
+};
