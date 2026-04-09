b05804536d256d9067520b0d49fc764505d0c4cb
Emarc480 <marcusemma480@gmail.com>
2026-04-09 22:08:51 +0300

    fixed the crash on reload issue

--- Academic_Issue_Tracking_System/Academic_Issue_Tracking_System/settings.py
+++ Academic_Issue_Tracking_System/Academic_Issue_Tracking_System/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'Academic_Issue_Tracking_System.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'static', 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'static', 'frontend'),
+]
 
 STATIC_URL = 'static/'
 
