4b3e9ab565d3d772cb2dd0e26d79eda67e339e6c
Emarc480 <marcusemma480@gmail.com>
2026-04-16 15:46:36 +0300

    Configured CORS

--- Internship_Logging_and_Evaluation_System/ILES/ILES/settings.py
+++ Internship_Logging_and_Evaluation_System/ILES/ILES/settings.py
@@ -107,6 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
