c1ebc3ee8899286b6c53a83002e5cfd3de347f2f
aojangole <arikolojangole@gmail.com>
2026-04-17 02:07:48 +0300

    created CustomUser

--- Internship_Logging_and_Evaluation_System/ILES/backend/models.py
+++ Internship_Logging_and_Evaluation_System/ILES/backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Student_Intern'),
+        ('workplace_supervisor', 'Workplace_Supervisor'),
+        ('academic_supervisor', 'Academic_Supervisor'),
+        ('intern_admin', 'Internship_Administrator'),
+    ]
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
