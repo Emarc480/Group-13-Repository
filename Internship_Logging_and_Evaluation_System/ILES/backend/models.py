ffb41bd8e7cc3bbd85525e3e4e5d02a8ed1a3e87
aojangole <arikolojangole@gmail.com>
2026-04-17 02:15:02 +0300

    created internship placement model

--- Internship_Logging_and_Evaluation_System/ILES/backend/models.py
+++ Internship_Logging_and_Evaluation_System/ILES/backend/models.py
@@ -10,3 +10,11 @@ class CustomUser(AbstractUser):
         ('intern_admin', 'Internship_Administrator'),
     ]
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+
+class InternshipPlacement(models.Model):
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='placements')
+    student_no = models.IntegerField(max_length=20)
+    company_name = models.CharField(max_length=45)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    workplace_supervisor = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, related_name='supervised_placements')
