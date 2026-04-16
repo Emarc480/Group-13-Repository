f1f1d31a0935a92579752aece22130a7dddbf016
MukisaPercy <mukisapercy4@gmail.com>
2026-04-16 18:54:01 +0300

    Re-apply user model settings

--- ILES_Project/iles/iles_backend/users/models.py
+++ ILES_Project/iles/iles_backend/users/models.py
@@ -2,16 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # Define role choices based on the ILES Core Roles
     ROLE_CHOICES = (
-        ('STUDENT', 'Student Intern'),
+        ('STUDENT', 'Student'),
         ('WORKPLACE_SUP', 'Workplace Supervisor'),
         ('ACADEMIC_SUP', 'Academic Supervisor'),
-        ('ADMIN', 'Internship Administrator'),
+        ('ADMIN', 'Internship Coordinator'),
     )
-    
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='STUDENT')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.username} ({self.role})"
+        return f"{self.username} ({self.get_role_display()})"
