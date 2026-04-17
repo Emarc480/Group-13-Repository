eb4db43ccf9313c14026590c76ebf031e0a072b1
aojangole <arikolojangole@gmail.com>
2026-04-17 03:57:10 +0300

    renamed check to condition in internship placement constraint

--- Internship_Logging_and_Evaluation_System/ILES/backend/models.py
+++ Internship_Logging_and_Evaluation_System/ILES/backend/models.py
@@ -22,7 +22,7 @@ class InternshipPlacement(models.Model):
     class Meta:
         constraints = [
             models.CheckConstraint(
-                check=models.Q(end_date__gt=models.F('start_date')),
+                condition=models.Q(end_date__gt=models.F('start_date')),
                 name='end_after_start'
             )]
         
