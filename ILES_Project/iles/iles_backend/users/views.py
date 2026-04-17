388df81b013c21fd0ffbd87c1bda0d1a5c0efe8d
MukisaPercy <mukisapercy4@gmail.com>
2026-04-17 14:39:48 +0300

    handles GET (viewing logs), POST (creating logs), and PUT (updating logs).

--- ILES_Project/iles/iles_backend/users/views.py
+++ ILES_Project/iles/iles_backend/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import WeeklyLog, InternshipPlacement, Evaluation
+from .serializers import WeeklyLogSerializer, InternshipPlacementSerializer, EvaluationSerializer
 
-# Create your views here.
+class WeeklyLogViewSet(viewsets.ModelViewSet):
+    serializer_class = WeeklyLogSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # This ensures a student only sees THEIR own logs
+        return WeeklyLog.objects.filter(placement__student=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically link the log to the student's active placement
+        placement = InternshipPlacement.objects.get(student=self.request.user)
+        serializer.save(placement=placement)
+
+class EvaluationViewSet(viewsets.ModelViewSet):
+    queryset = Evaluation.objects.all()
+    serializer_class = EvaluationSerializer
+    permission_classes = [permissions.IsAuthenticated]
