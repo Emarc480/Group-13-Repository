33dcc655055a27bb947d2fdb1a9cfb1ada588405
Emarc480 <marcusemma480@gmail.com>
2026-04-20 08:39:56 +0300

    Added Routing and pages

---
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Loginpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Loginpage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Please enter your credentials to log in.</p>
+    </div>
+  );
+}
+
+export default Loginpage;
