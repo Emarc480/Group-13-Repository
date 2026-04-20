65fc16ae16540d617446ae116407284ce980e7ae
Emarc480 <marcusemma480@gmail.com>
2026-04-20 08:57:43 +0300

    fixed a few issues

--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/App.jsx
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-
+import Loginpage from './pages/Loginpage'
+import Dashboard from './pages/Dashboard'
+import Homepage from './pages/Homepage'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route />
-        <Route />
+        <Route path='home' element={<Homepage />} />
+        <Route path='login' element={<Loginpage />} />
+        <Route path='dashboard' element={<Dashboard />} />
       </Routes>
     </Router>
   );
