02ac596448e056e44759a59b4603e098a5ac30d3
Emarc480 <marcusemma480@gmail.com>
2026-04-20 14:42:26 +0300

    Created usestates for login and signup

--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Loginpage.jsx
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Loginpage.jsx
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { User as U, KeyRound as KR, Mail, LockKeyhole as LK } from 'lucide-react';
 
 function Loginpage() {
+  const [showLogin, setShowLogin] = React.useState(true);
+
+
   return (
-    <div>
-      <h1>Login</h1>
-      <p>Please enter your credentials to log in.</p>
-    </div>
+    <>
+      <div className="Header">
+        <h2>Login</h2>
+      </div>
+      {showLogin ? (
+        <div className="loginForm">
+          <form>
+            <div>
+              <U />
+              <input placeholder="Username..."></input>
+            </div>
+            <div>
+              <KR />
+              <input placeholder="Password..."></input>
+            </div>
+          </form>
+          <div className="Login">
+            <button>Login</button>
+          </div>
+          <div className="SignUpPrompt">
+            <p>
+              New here?
+              <a href="#" onClick={() => setShowLogin(false)}>
+                Sign up
+              </a>
+            </p>
+          </div>
+        </div >
+      ) : (
+        <div className="SignUpForm">
+          <form>
+            <div>
+              <U />
+              <input placeholder="Username..."></input>
+            </div>
+            <div>
+              <Mail />
+              <input placeholder="Email..."></input>
+            </div>
+            <div>
+              <KR />
+              <input placeholder="Password..."></input>
+            </div>
+            <div>
+              <LK />
+              <input placeholder="Confirm Password..."></input>
+            </div>
+          </form>
+          <div className="SignUp">
+            <button>Sign Up</button>
+          </div>
+          <div className="LoginPrompt">
+            <p>
+              Already have an account?
+              <a href="#" onClick={() => setShowLogin(true)}>
+                Login
+              </a>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
