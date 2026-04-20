ce94195f625b2cecb6c302b5419742db5e87f27f
Emarc480 <marcusemma480@gmail.com>
2026-04-20 09:20:34 +0300

    Fixed routing and added components

--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Homepage.jsx
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Homepage.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Homepage() {
-  return (
-    <div>
-      <h1>Welcome to the Homepage!</h1>
-      <p>This is the main landing page of our application.</p>
-    </div>
-  );
+    const navigate = useNavigate();
+    const handleLoginClick = () => {
+        navigate('/login');
+    };
+
+    return (
+        <>
+            <div>
+                <h1>Welcome to the Homepage!</h1>
+                <p>This is the main landing page of our application.</p>
+                <p>Please click below to login or register.</p>
+            </div>
+            <div>
+                <button onClick={handleLoginClick}>Log in</button>
+            </div>
+        </>
+    );
 }
 
 export default Homepage;
