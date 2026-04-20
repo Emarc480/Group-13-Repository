533055699fca856620959fdd5ba548ca4aa5b540
Mukisa Percy <mukisapercy4@gmail.com>
2026-04-20 11:05:14 +0300

    Delete ILES_Project directory

--- ILES_Project/iles/iles_backend/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation
-
-admin.site.register(User)
-admin.site.register(InternshipPlacement)
-admin.site.register(WeeklyLog)
-admin.site.register(EvaluationCriteria)
-admin.site.register(Evaluation)
