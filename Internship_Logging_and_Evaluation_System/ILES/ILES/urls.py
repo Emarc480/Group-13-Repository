0acf28bf4a8b6a3594a45a5b5524b18f2d38879f
MukisaPercy <mukisapercy4@gmail.com>
2026-04-21 19:09:38 +0300

    Connecting addresses in the code.

--- Internship_Logging_and_Evaluation_System/ILES/ILES/urls.py
+++ Internship_Logging_and_Evaluation_System/ILES/ILES/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
+from backend.views import InternshipPlacementViewset, WeeklyLogViewset, EvaluationViewset
+
+router = DefaultRouter()
+router.register(r'placements', InternshipPlacementViewset)
+router.register(r'logs', WeeklyLogViewset, basename='weeklylog')
+router.register(r'evaluations', EvaluationViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
