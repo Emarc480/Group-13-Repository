8e4146c9f9514072494c9f2a447eadbbf1e7279b
Emarc480 <marcusemma480@gmail.com>
2026-04-22 00:44:41 +0300

    Adding dashboard access as per role

--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Dashboard.jsx
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 
+import AcademicSup from "../components/AcademicSup";
+import InternshipAdmin from "../components/InternshipAdmin";
+import WorkplaceSup from "../components/WorkplaceSup";
+import StudentDashboard from "../components/StudentDashboard";
+
 function Dashboard() {
+  const role = localStorage.getItem("role");
+
+  const RenderView = () => {
+    switch (role) {
+      case "academic_supervisor":
+        return <AcademicSup />;
+      case "internship_admin":
+        return <InternshipAdmin />;
+      case "workplace_supervisor":
+        return <WorkplaceSup />;
+      case "student":
+        return <StudentDashboard />;
+      default:
+        return <p>Invalid role</p>;
+    }
+  };
+
   return (
-    <div>
-      <p>nothing to display</p>
-    </div>
+    <>
+      <div>
+        <p>Nav</p>
+      </div>
+      <div>{RenderView()}</div>
+    </>
   );
 }
 
