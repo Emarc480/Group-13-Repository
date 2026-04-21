a76878e229851054b2d7516a7ebd37302c2acc11
Arnold Henry Oyuru <henryarnoldoyuru@gmail.com>
2026-04-21 08:12:17 +0300

    fix: correct login URL in authService from token/ to auth/login/

--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/services/authService.js
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/services/authService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'http://127.0.0.1:8000/api/';
 
 export const login = async (username, password) => {
-    const response = await axios.post(`${API_URL}token/`, { username, password });
+    const response = await axios.post(`${API_URL}auth/login/`, { username, password });
     if (response.data.access) {
         localStorage.setItem('token', response.data.access);
         // We'll also need to decode the token or hit a /me endpoint to get the role
