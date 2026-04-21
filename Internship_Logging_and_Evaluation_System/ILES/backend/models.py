05b2cb0b9020f5d7534fa29ff476b224e4cac303
MukisaPercy <mukisapercy4@gmail.com>
2026-04-21 18:33:43 +0300

    Defiining roles and users.

--- Internship_Logging_and_Evaluation_System/ILES/backend/models.py
+++ Internship_Logging_and_Evaluation_System/ILES/backend/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('STUDENT', 'Student'),
+        ('WORKPLACE_SUP', 'Workplace Supervisor'),
+        ('ACADEMIC_SUP', 'Academic Supervisor'),
+        ('COORDINATOR', 'Internship Coordinator'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='STUDENT')
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    organization = models.CharField(max_length=100, blank=True, null=True)
