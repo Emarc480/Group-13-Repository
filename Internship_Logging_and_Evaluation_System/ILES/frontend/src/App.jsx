2ed1c0ff01fc834d5c1c325ae8fc80f3ee4c1850
Arnold Henry Oyuru <henryarnoldoyuru@gmail.com>
2026-04-21 09:53:56 +0300

    feat: implement ProtectedRoute to guard dashboard routes from unauthenticated access

--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/App.jsx
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import ProtectedRoute from './components/ProtectedRoute'
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Loginpage from './pages/Loginpage'
@@ -10,10 +11,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/login' element={<Loginpage />} />
-        <Route path='/dashboard/student' element={<Dashboard />} />
-        <Route path='/dashboard/supervisor' element={<Dashboard />} />
-        <Route path='/dashboard/academic' element={<Dashboard />} />
-        <Route path='/dashboard/admin' element={<Dashboard />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path='/dashboard/student' element={<Dashboard />} />
+          <Route path='/dashboard/supervisor' element={<Dashboard />} />
+          <Route path='/dashboard/academic' element={<Dashboard />} />
+          <Route path='/dashboard/admin' element={<Dashboard />} />
+        </Route>
       </Routes>
     </Router>
   );
--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/components/ProtectedRoute.jsx
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = ({ allowedRoles, userRole }) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('access_token');
 
     // 1. Check if logged in
     if (!token) {
