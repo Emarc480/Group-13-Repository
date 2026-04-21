093a61dfa9bb7ec970fddbfe44aaf3d04d3a7204
MukisaPercy <mukisapercy4@gmail.com>
2026-04-21 19:06:37 +0300

    Added user permissions

--- Internship_Logging_and_Evaluation_System/ILES/backend/views.py
+++ Internship_Logging_and_Evaluation_System/ILES/backend/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import InternshipPlacement, WeeklyLog, Evaluation, EvaluationCriteria
+from .serializers import *
 
-# Create your views here.
+class IsInternAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'COORDINATOR'
+
+class IsStudent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'STUDENT'
+
+class InternshipPlacementViewset(viewsets.ModelViewSet):
+    queryset = InternshipPlacement.objects.all()
+    serializer_class = InternshipPlacementSerializer
+    permission_classes = [permissions.IsAuthenticated, IsInternAdmin]
+
+class WeeklyLogViewset(viewsets.ModelViewSet):
+    serializer_class = WeeklyLogSerializer
+    permission_classes = [permissions.IsAuthenticated, IsStudent]
+
+    def get_queryset(self):
+        return WeeklyLog.objects.filter(placement__student=self.request.user)
+
+class EvaluationViewset(viewsets.ModelViewSet):
+    queryset = Evaluation.objects.all()
+    serializer_class = EvaluationSerializer
+    permission_classes = [permissions.IsAuthenticated]
