c159c47bcbdbe671a8f7b79355d4a91149e63def
Emarc480 <marcusemma480@gmail.com>
2026-04-22 00:11:17 +0300

    Added the dashboard components

--- Internship_Logging_and_Evaluation_System/ILES/ILES/settings.py
+++ Internship_Logging_and_Evaluation_System/ILES/ILES/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-93=3rvest$+%%+kpatlyan%hj((ok6!^7^yep8f3urx=^z$au6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'ILES.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'NAME': 'ILES_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Emma1234',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Dashboard.jsx
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Dashboard.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
-      <p>This is the user dashboard.</p>
+      <p>nothing to display</p>
     </div>
   );
 }
