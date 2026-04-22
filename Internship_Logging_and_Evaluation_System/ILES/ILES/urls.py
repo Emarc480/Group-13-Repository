7d02ad138579be67cce5a76a48984408c046dae1
MukisaPercy <mukisapercy4@gmail.com>
2026-04-22 20:49:46 +0300

    refactor: decouple main urls from backend api logic using include

--- Internship_Logging_and_Evaluation_System/ILES/ILES/urls.py
+++ Internship_Logging_and_Evaluation_System/ILES/ILES/urls.py
@@ -17,17 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView
-from backend.views import InternshipPlacementViewset, WeeklyLogViewset, EvaluationViewset
-
-router = DefaultRouter()
-router.register(r'placements', InternshipPlacementViewset)
-router.register(r'logs', WeeklyLogViewset, basename='weeklylog')
-router.register(r'evaluations', EvaluationViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/', include('backend.urls')),
 ]
