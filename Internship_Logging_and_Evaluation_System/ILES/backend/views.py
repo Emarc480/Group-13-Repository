b0b929841da911204d2c8ad04980dea965327eea
aojangole <arikolojangole@gmail.com>
2026-04-23 08:59:12 +0300

    created the customtokenobtainpairview

--- Internship_Logging_and_Evaluation_System/ILES/backend/views.py
+++ Internship_Logging_and_Evaluation_System/ILES/backend/views.py
@@ -13,7 +13,12 @@ from .serializers import (
     EvaluationSerializer,
     EvaluationCriteriaSerializer
 )
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
 # Create your views here.
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
