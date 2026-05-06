7f7fa0366f14ccee295312772458b09a7701926f
Emarc480 <marcusemma480@gmail.com>
2026-05-06 20:57:13 +0300

    Added the navbar styling

--- Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Dashboard.jsx
+++ Internship_Logging_and_Evaluation_System/ILES/frontend/src/pages/Dashboard.jsx
@@ -52,12 +52,25 @@ function Dashboard() {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }} >
-      <div>
+    <div style={{ display: 'flex', height: '100vh' }} >
+
+      {/* Sidebar */}
+      <div style={{
+        width: '150px',
+        padding: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+        borderRight: '1px solid #ccc'
+      }}>
         <button>Profile</button>
         <button onClick={() => { handleLogout() }}>Logout</button>
       </div>
-      <div>{RenderView()}</div>
+
+      {/* Main Content */}
+      <div style={{ flex: 1, padding: '20px' }}>
+        {RenderView()}
+      </div>
     </div>
   );
 }
